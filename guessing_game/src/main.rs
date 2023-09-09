44dbfabd9b2a2ba5ac6f1d41b62922c9f2d6ed51
syureu <tor01237@gmail.com>
2023-09-09 14:36:58 +0900

    2. Programming a Guessing Game - Comparing the Guess to the Secret Number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,7 +3,22 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    // --snip--
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {guess}");
 
@@ -12,4 +27,5 @@ fn main() {
         Ordering::Greater => println!("Too big!"),
         Ordering::Equal => println!("You win!"),
     }
+
 }

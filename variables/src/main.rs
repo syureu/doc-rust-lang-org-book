8c954b0d1674260ad50aa86aed302981cda5b71c
syureu <tor01237@gmail.com>
2023-09-09 14:56:35 +0900

    3. Common Programming Concepts - 3.2 Data Types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,15 +1,3 @@
 fn main() {
-    let x = 5;
-
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x is: {x}");
-
-    let mut spaces = "   ";
-    spaces = spaces.len();
+    let guess: u32 = "42".parse().expect("Not a number!");
 }

10948b4b10e2d46272e25f2ed59e17db598b82eb
syureu <tor01237@gmail.com>
2023-09-09 14:41:33 +0900

    2. Programming a Guessing Game - Handling Invalid Input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -16,11 +16,14 @@ fn main() {
         let mut guess = String::new();
     
         io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-    
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         println!("You guessed: {guess}");
     
         match guess.cmp(&secret_number) {

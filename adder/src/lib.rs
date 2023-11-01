15e45d8b5bc1f9323f1702e26748da592e9786a9
syureu <tor01237@gmail.com>
2023-11-01 19:55:29 +0900

    11.1 How to Write Tests
    WIP

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -6,7 +6,7 @@ struct Rectangle {
 
 impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+        self.width < other.width && self.height > other.height
     }
 }
 

1b0c9146495eb642748c93964d84aa5da9fb9a98
syureu <tor01237@gmail.com>
2023-11-03 17:16:12 +0900

    11.1 How to Write Tests
    FIN

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,32 +1,11 @@
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}.",
-                value
-            );
-        }
-
-        Guess { value }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    #[should_panic(expected = "less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }

559c0fef4a1b64052e233cba60f4b5e68654e72f
syureu <tor01237@gmail.com>
2023-11-03 16:59:48 +0900

    11.1 How to Write Tests
    WIP

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -6,12 +6,12 @@ impl Guess {
     pub fn new(value: i32) -> Guess {
         if value < 1 {
             panic!(
-                "Guess value must be greater than or equal to 1, got {}.",
+                "Guess value must be less than or equal to 100, got {}.",
                 value
             );
         } else if value > 100 {
             panic!(
-                "Guess value must be less than or equal to 100, got {}.",
+                "Guess value must be greater than or equal to 1, got {}.",
                 value
             );
         }

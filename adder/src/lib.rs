0a1cb3baf31ec03c98ac188ed6fd0d411bf6c2fb
syureu <tor01237@gmail.com>
2023-11-03 16:57:31 +0900

    11.1 How to Write Tests
    WIP

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -4,7 +4,7 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
+        if value < 1 {
             panic!("Guess value must be between 1 and 100, got {}.", value);
         }
 
